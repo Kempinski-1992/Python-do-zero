2f2224a5195778de6b26ebbb9a12f9e78349a95e
Kempinski-1992 <79817951+Kempinski-1992@users.noreply.github.com>
2021-03-06 01:28:37 -0300

    Instruções do livro: Python do Zero

---
+++ .vscode/Leandro.py
@@ -0,0 +1,8 @@
+variavel1 = input('Digite um número: ');
+
+if variavel1 >= str(0):
+    print ('Número Positivo.')
+    print (f'O número é: {variavel1}')
+else:
+    print ('Número Negativo')
+    print (f'O número é: {variavel1}')
