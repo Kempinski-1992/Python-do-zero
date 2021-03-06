9900b73b7fb0f3c3fc06f1acff3c77e71704a772
Kempinski-1992 <79817951+Kempinski-1992@users.noreply.github.com>
2021-03-06 12:56:36 -0300

    Continuação do Livro Python do Zero.

---
+++ .vscode/Outras atividades.py
@@ -0,0 +1,50 @@
+print('Olá Mundo!!!')
+
+variavel_tipo_string = 'Conjunto de Caracteres alfanuméricos'
+
+variavel_tipo_int = 12 #número inteiro
+
+variavel_tipo_float = 15.3 #número com casas decimais
+
+variavel_tipo_bool = True #Booleano (verdadeiro ou falso)
+
+variavel_tipo_lista = [1, 2, 'Paulo', 'Maria', True]
+
+variavel_tipo_dicionario = {'nome':'Fernando', 'idade':31,}
+
+variavel_tipo_tupla = ('Ana', 'Fernando', 3)
+
+variavel_tipo_comentario = """Um comentário atribuído a uma variável deixa de ser apenas um comentário, vira frase!!!"""
+
+nome = 'Maria'
+#Maria é a nova funcionária
+#Todo texto escrito após o sustenido ou entre aspas triplas o interpretador irá ignorar!
+
+print('Bem vinda Maria!!!')
+#Acima está uma mensagem de boas vindas a Maria.
+
+'''Este programa está sendo escrito para que Maria, a nova funcionária, comece  a se integrar com o sistema.'''
+
+#caso queira que um comentário seja visível é possível associá-lo a uma variável. Ex:
+
+Comentario1 = '''Exemplo de comentário 
+e agora sim será lido 
+pelo interpretador!!!'''
+print(Comentario1)
+
+var1 = 2043
+var2 = 432
+
+soma = var1 + var2
+
+print(soma)
+
+#Fazendo operação soma diretamento no comando print
+
+var3 = 236
+var4 = 426
+var5 = 835
+
+#soma = var3 + var5
+
+print(var3 + var5)
